a6ec15b0c0de991948bde0795181080b91ca4744
mpourzad <mahmoud1535@gmail.com>
2015-08-25 21:30:33 +0200

    Adding new files to init project

---
+++ Hello.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html dir=rtl>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>webbook</title>
+
+<html>
+<body>
+<center>
+<table width="200" border="0">
+<tr>
+  
+  <!--M_start color to bg, text, link and vlink-->
+  <BODY bgcolor ="######" TEXT ="ffffff" LINK ="FFFACD" vlink ="ffffff">
+  <!--M_slut color to bg, text, link and vlink-->
+  
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+	<center><h1>!webbook.. snart tillbaka</h1></center>
+    <td><center><img src="image003.gif"></center></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+     </tr>
+  </table>
+<EMBED SRC="alex.wma" LOOP="TRUE" HIDDEN="TRUE">
+<NOEMBED>
+<BGSOUND SRC="alex.wma" LOOP="INFINITE">
+</NOEMBED>
+</EMBED>
+
+</center>
+</body>
+</html>
